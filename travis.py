46e7cf7cf50b33e9b9d2aef63553893663da7eb5
shizzy13 <shizzy13@abv.bg>
2018-05-22 16:53:59 +0200

    Path modifications

--- travis.py
+++ travis.py
@@ -1,31 +1,36 @@
-import os, sys, re
+import os, sys, re, json
 
 #Open folder in Optimization
-opt_path = r"C:\Users\neli\Desktop\TESTYAML\bsp_data_repository\optimizations"
-opt_folder = os.listdir(opt_path)
+repository = os.path.dirname(os.path.abspath(__file__))
+optimizations = os.path.join(repository, "optimizations")
+
+print repository, "\n\n"
+print optimizations, "\n\n"
 
 #Attempt to extract files in same directory
-for item in opt_folder:
-        extension = ".zip"
-        if item.endswith(extension):
-            zipfile.ZipFile.extractall(os.path.join(opt_path, item))
-     
-        config_path = os.path.join(opt_path, item, item, "config\morph.json")
-        print config_path
-        
-#Attempt to open morph.json file and get the string that I need      
-        with open(config_path,'r') as f:
-            data=[]
-            flag=False
-            for char in f:
-                if ' "' in line:
-                    flag=True
-                if flag:
-                    data.append(char)
-                if '"}' in line:
-                    flag=False
+for folder in os.listdir(optimizations):
 
-            print ''.join(data)
+        if (re.match('CA1', folder)): #Avoid README file
+                curr_folder = os.path.join(optimizations, folder)
+#                print folder
+                extension = ".zip"
+                if folder.endswith(extension):
+                        zipfile.ZipFile.extractall(os.path.join(optimizations, folder))
+                
+     
+                config = os.path.join(optimizations, folder, folder, "config\morph.json")
+#                print config
+     
+"""#Attempt to read .json file     
+        with open(config) as json_data:      
+                d = json.load(json_data)
+                print(d)
+"""
+
+
+
+    
+
             
 
 
