3cf0249315090fff066e7e8ec9c2b33b1e7ec88e
shizzy13 <shizzy13@abv.bg>
2018-05-23 11:56:16 +0200

    Update

--- travis.py
+++ travis.py
@@ -4,19 +4,22 @@ import os, sys, re, json
 repository = os.path.dirname(os.path.abspath(__file__))
 optimizations = os.path.join(repository, "optimizations")
 
-#print "Print repository: \n", repository, "\n\n"
-#print "Print optimizations: \n", optimizations, "\n\n"
+print "Print repository: \n", repository, "\n\n"
+print "Print optimizations: \n", optimizations, "\n\n"
 
 #Attempt to extract files in same directory
 for folder in os.listdir(optimizations):
     if (re.match('CA1', folder)): #Avoid README file
         curr_folder = os.path.join(optimizations, folder)
-#        print curr_folder
+        print curr_folder
         extension = ".zip"
+        print os.listdir(os.path.join(optimizations, folder))
+        print os.getcwd()
         if folder.endswith(extension):
             os.chdir(os.path.join(optimizations, folder))
-            zipfile.ZipFile.extractall(curr_folder)
+            zipfile.ZipFile.extractall(folder)
             os.chdir(os.path,join('..','..'))
+        print os.getcwd()
         print os.listdir(os.path.join(optimizations, folder))
            
 #Attempt to read .json file
