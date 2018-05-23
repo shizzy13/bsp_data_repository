9ba90db5d85a9dd649c440be63fb52998b9d33d6
shizzy13 <shizzy13@abv.bg>
2018-05-23 13:57:10 +0200

    Hello print

--- travis.py
+++ travis.py
@@ -11,18 +11,20 @@ optimizations = os.path.join(repository, "optimizations")
 for folder in os.listdir(optimizations):
     if (re.match('CA1', folder)): #Avoid README file
         curr_folder = os.path.join(optimizations, folder)
-        print curr_folder
+#        print curr_folder
         extension = ".zip"
-        print os.listdir(os.path.join(optimizations, folder))
-        print os.getcwd()
+#        print os.listdir(os.path.join(optimizations, folder))
+#        print os.getcwd()
+        print "Hello"
         if folder.endswith(extension):
+            print "bye"
             os.chdir(os.path.join(optimizations, folder))
             zip_ref = zipfile.ZipFile(folder, 'r')
             zip_ref.extractall('.')
             zip_ref.close() 
             os.chdir(os.path,join('..','..'))
-        print os.getcwd()
-        print os.listdir(os.path.join(optimizations, folder))
+#        print os.getcwd()
+#        print os.listdir(os.path.join(optimizations, folder))
            
 #Attempt to read .json file
 for folder in os.listdir(optimizations):
