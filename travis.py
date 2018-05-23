ca41418c4b3e9b42abbbecafe4fd8432938e8003
shizzy13 <shizzy13@abv.bg>
2018-05-23 17:01:30 +0200

    read .json success

--- travis.py
+++ travis.py
@@ -11,11 +11,11 @@ print optimizations
 for folder in os.listdir(optimizations):
     if (re.match('CA1', folder)): #Avoid README file
         curr_folder = os.path.join(optimizations, folder)
-        print "###curr_folder###", curr_folder
-        print "###os.listdir(os.path.join(optimizations, folder))###", os.listdir(os.path.join(optimizations, folder))
-        print "###os.getcwd()###", os.getcwd()
+        print "##curr_folder##", curr_folder
+        print "##os.listdir(os.path.join(optimizations, folder))##", os.listdir(os.path.join(optimizations, folder))
+        print "##os.getcwd()##", os.getcwd()
         print "outside"
-        print "###folder###", folder
+        print "##folder##", folder
         for files in os.listdir(os.path.join(optimizations, folder)):
             if files.endswith('.zip'):
                 print "insidee"
@@ -24,20 +24,20 @@ for folder in os.listdir(optimizations):
                 zip_ref.extractall('.')
                 zip_ref.close() 
                 os.chdir(os.path.join('..','..'))
-            print "###os.getcwd()###", os.getcwd()
-            print "###os.listdir(os.path.join(optimizations, folder))###", os.listdir(os.path.join(optimizations, folder))
+            print "##os.getcwd()##", os.getcwd()
+            print "##os.listdir(os.path.join(optimizations, folder))##", os.listdir(os.path.join(optimizations, folder))
            
 #Read .json file
         for files in os.listdir(os.path.join(optimizations, folder)):
-            print "###os.getcwd()###", os.getcwd()
+            print "##os.getcwd()##", os.getcwd()
             os.chdir(os.path.join(optimizations, folder, folder, "config"))
-            print "###(os.listdir('.'))###",(os.listdir('.'))
+            print "##(os.listdir('.'))##",(os.listdir('.'))
 #            os.chdir(os.path.join('..','..'))
             with open("morph.json") as json_file:
                 json_data = json.load(json_file)
-            print "(json_data)###########", (json_data)
+            print "##(json_data)##", (json_data)
             os.chdir(os.path.join('..','..'))
-            print "os.getcwd()###########", os.getcwd()
+            print "##os.getcwd()##", os.getcwd()
 
 
 
