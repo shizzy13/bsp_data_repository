63ee8054db9209464b27eed5245c3343088925f4
shizzy13 <shizzy13@abv.bg>
2018-05-23 12:01:22 +0200

    Update

--- travis.py
+++ travis.py
@@ -4,30 +4,34 @@ import os, sys, re, json
 repository = os.path.dirname(os.path.abspath(__file__))
 optimizations = os.path.join(repository, "optimizations")
 
-print "Print repository: \n", repository, "\n\n"
-print "Print optimizations: \n", optimizations, "\n\n"
+#print "Print repository: \n", repository, "\n\n"
+#print "Print optimizations: \n", optimizations, "\n\n"
 
 #Attempt to extract files in same directory
 for folder in os.listdir(optimizations):
     if (re.match('CA1', folder)): #Avoid README file
         curr_folder = os.path.join(optimizations, folder)
-        print curr_folder
+#        print curr_folder
         extension = ".zip"
-        print os.listdir(os.path.join(optimizations, folder))
-        print os.getcwd()
+#        print os.listdir(os.path.join(optimizations, folder))
+#        print os.getcwd()
         if folder.endswith(extension):
             os.chdir(os.path.join(optimizations, folder))
             zipfile.ZipFile.extractall(folder)
             os.chdir(os.path,join('..','..'))
-        print os.getcwd()
-        print os.listdir(os.path.join(optimizations, folder))
+ #       print os.getcwd()
+#        print os.listdir(os.path.join(optimizations, folder))
            
 #Attempt to read .json file
 for folder in os.listdir(optimizations):
+    print os.getcwd()
     os.chdir(os.path.join(optimizations, folder, folder, "config"))
     with open("morph.json") as json_file:
         json_data = json.load(json_file)
     print(json_data)
+    os.chdir(os.path,join('..','..'))
+    print os.getcwd()
+
 
 
 
