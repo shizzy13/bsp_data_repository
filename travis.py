914f64aaaae5eafb8a89e27f0194fe8fa9aa7b41
shizzy13 <shizzy13@abv.bg>
2018-05-31 14:50:46 +0200

    checks 7,8

--- travis.py
+++ travis.py
@@ -1,10 +1,12 @@
 import os, sys, re, json, zipfile, filecmp, getopt, os.path,  filecmp
 print "Check 1: File in 'morphology' folder has the same name as the value of the key in 'morph.json' in 'config' folder\n", \
 "Check 2: Only 1 file is present in 'morphology' folder \n", \
-"Check 3: features.json, morph.json, parameters.json, protocols.json files are present in config \n", \
+"Check 3: features.json, morph.json, parameters.json, protocols.json files are present in 'config' folder \n", \
 "Check 4: The same key is used in all .json files in 'config' folder \n", \
 "Check 5: All files with the same name in all 'mechanisms' folders are exact copies \n", \
 "Check 6: All the folders have the same structure \n", \
+"Check 7: same 4 files are present in 'model' folder \n", \
+"Check 8: same 2 files are present in 'tools' folder \n"
 
 def check_one (name,morph_data):
     same_name = 0
@@ -88,6 +90,29 @@ def check_six (check_folder):
                 return False
             else:
                 return True
+
+def check_seven(model_list):
+    check_seven = ['analysis.py', 'evaluator.py', 'template.py', '__init__.py']
+    if check_seven == model_list:
+        print "Check 7: Success!"
+    else:
+        print "Check 7: Fail!"
+        print "    Files present in 'model' folder:"
+        for i in range (0,len(model_list)):
+            print "       ", model_list[i]
+    return check_seven == model_list
+
+def check_eight(tools_list):
+    check_eight = ['get_stats.py', 'task_stats.py']
+    if check_eight == tools_list:
+        print "Check 8: Success!"
+    else:
+        print "Check 8: Fail!"
+        print "    Files present in 'tools' folder:"
+        for i in range (0,len(tools_list)):
+            print "       ", tools_list[i]
+    return check_eight == tools_list
+
 def unique_key(list1):
     unique_list = []
     repeat_list = []
@@ -107,19 +132,18 @@ optimizations = os.path.join(repository, "optimizations")
 for folder in os.listdir(optimizations):
     if (not re.match('README', folder)): #Avoid README file
         curr_folder = os.path.join(optimizations, folder)
-        for files in os.listdir(curr_folder):
+        """for files in os.listdir(curr_folder):
             if files.endswith('.zip'):
                 os.chdir(curr_folder)
                 zip_ref = zipfile.ZipFile(files, 'r')
                 zip_ref.extractall('.')
                 zip_ref.close() 
-                os.chdir(os.path.join('..','..'))
+                os.chdir(os.path.join('..','..'))"""
 
 #Read .json file
         for files in os.listdir(curr_folder):
             if (files == folder):
                 os.chdir(os.path.join(optimizations, folder, folder, "config"))
-                config_list = os.listdir('.')
                 with open("morph.json") as json_file:
                     morph_data = json.load(json_file)
                     
@@ -128,6 +152,7 @@ for folder in os.listdir(optimizations):
                 print "\n\n", folder
                 check_one(name,morph_data)
                 check_two(name)
+                config_list = os.listdir(os.path.join(optimizations, folder, folder, "config"))
                 bool_three = check_three(config_list)
                 if bool_three is True:   
                     check_four_boolean = (check_four("morph.json") == check_four("features.json") == check_four("parameters.json") == check_four("protocols.json"))
@@ -148,8 +173,13 @@ for folder in os.listdir(optimizations):
                 else:
                     print "Check 4: Cannot run because Check 3 failed."
                 os.chdir(os.path.join('..','..','..'))
+                model_list = os.listdir(os.path.join(optimizations, folder, folder, "model"))
+                check_seven(model_list)
+                tools_list = os.listdir(os.path.join(optimizations, folder, folder, "tools"))
+                check_eight(tools_list)
     
 check_five()
 check_six_bool = (check_six("checkpoints") == check_six("config") == check_six("figures") == check_six("mechanisms") == check_six("model") == check_six("morphology") == check_six("tools"))
 if check_six_bool is True:
     print "\nCheck 6: Success!"
+
